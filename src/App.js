c872a6bc7032c047b16497d0fd510d69177d037c
Parveen Kumar <jordyparv@gmail.com>
2024-05-18 20:12:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React from 'react'
 function App() {
+  /**
+   * useState
+   * useEffect
+   * useRef
+   * memo
+   * useMemo
+   * 
+   * 
+   */
+
+  const [value, setValue] = React.useState(1)
+
+  console.log(value)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button>+</button>
+      <span>{'jordan'}</span>
+      <button>-</button>
     </div>
   );
 }
